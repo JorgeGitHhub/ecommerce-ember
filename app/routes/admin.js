0b4ecaecacc6374a465699088cb0e217f64d3a14
Marta Sanchez and Thomas Rivas <thomas@>
2016-11-03 15:43:00 -0700

    finish all the categories display and add an admin panel

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    return this.store.findAll('item');
+  },
   actions: {
   saveNewItem(params) {
       var newItem = this.store.createRecord('item', params);
